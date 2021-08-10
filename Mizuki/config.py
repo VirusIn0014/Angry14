60b2ad110b4f36fb04c9f5bcbc115bfc4d64807d
VirusIn0014 <81487024+VirusIn0014@users.noreply.github.com>
2021-08-10 15:45:29 +0530

    Update config.py

--- Mizuki/config.py
+++ Mizuki/config.py
@@ -20,6 +20,7 @@ class Config(object):
     TOKEN = "1931181190:AAFV2z-gZgOmRl5fjjVwv_o9GDrI8NEHew8"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "@Its_Vaib"
+    UPSTREAM_REPO = "https://github.com/VirusIn0014/Angry14"
     SUPPORT_CHAT = "AlwaysAngryCat"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001535254774
